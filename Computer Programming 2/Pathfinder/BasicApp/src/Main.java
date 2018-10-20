3e266db8d050f1c7c79bfde4bb76cc762226bdf1
Ben Botvinick <benbotvinick@gmail.com>
2018-10-19 22:23:27 -0400

    did some work

--- Computer Programming 2/Pathfinder/BasicApp/src/Main.java
+++ Computer Programming 2/Pathfinder/BasicApp/src/Main.java
@@ -8,35 +8,33 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.*;
 
-public class BasicGameApp implements Runnable, KeyListener, MouseListener {
+public class Main implements Runnable, KeyListener, MouseListener {
 
-	final int WIDTH = 1000;
-	final int HEIGHT = 700;
+	private final int WIDTH = 900;
+	private final int HEIGHT = 700;
 
-	JFrame frame;
-	Canvas canvas;
-	BufferStrategy bufferStrategy;
-	public Image image;
+	private BufferStrategy bufferStrategy;
+	private Image image;
 
-	private int xpos = 300;
+	private int xpos = 400;
 	private int ypos = 300;
-	private boolean left, right, up, down;
-	private int x, y;
-	private Grid grid = new Grid(7, 10);
+	private int xdest = 400;
+	private int ydest = 300;
+	private Grid grid = new Grid(7, 9);
 
 	public static void main(String[] args) {
-		BasicGameApp ex = new BasicGameApp();
+		Main ex = new Main();
 		new Thread(ex).start();
 	}
 
-	public BasicGameApp() {
-		frame = new JFrame("Basic Game");
+	private Main() {
+		JFrame frame = new JFrame("Pathfinder");
 
 		JPanel panel = (JPanel) frame.getContentPane();
 		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		panel.setLayout(null);
 
-		canvas = new Canvas();
+		Canvas canvas = new Canvas();
 		canvas.setBounds(0, 0, WIDTH, HEIGHT);
 		canvas.setIgnoreRepaint(true);
 
@@ -51,17 +49,14 @@ public class BasicGameApp implements Runnable, KeyListener, MouseListener {
 		frame.setVisible(true);
 
 		image = Toolkit.getDefaultToolkit().getImage(
-				"astronaut.png");
+				"images/sprite.jpg");
 
 		canvas.createBufferStrategy(2);
 		bufferStrategy = canvas.getBufferStrategy();
 
 		canvas.requestFocus();
-		// add stuff
-	}// BasicGameApp()
+	}
 
-
-	//thread
 	public void run() {
 
 		while (true) {
@@ -69,31 +64,24 @@ public class BasicGameApp implements Runnable, KeyListener, MouseListener {
 			render();
 
 			// move things
-			if (right)
-				xpos += 2;
-			if (left)
-				xpos -= 2;
-			if (up)
-				ypos -= 2;
-			if (down)
-				ypos += 2;
+			if (xpos != xdest && ypos % 100 == 0) {
+				xpos += Math.abs(xdest - xpos) / (xdest - xpos);
+			} else if (ypos != ydest && xpos % 100 == 0) {
+				ypos += Math.abs(ydest - ypos) / (ydest - ypos);
+			}
 
 			//sleep
 			try {
 				Thread.sleep(5);
-			} catch (InterruptedException e) {
+			} catch (InterruptedException ignored) {
 
 			}
 		}
 	}
 
-	//paints things on the screen using bufferStrategy
 	private void render() {
 		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
 		g.clearRect(0, 0, WIDTH, HEIGHT);
-		// g.fillRect((int) x, 0, 200, 200);
-//		g.drawImage(image, xpos, ypos, 100, 100, null);
-//		g.fillRect(x,y,22,22);
 
 		for (int x = 0; x < WIDTH / 100; x++) {
 			for (int y = 0; y < HEIGHT / 100; y++) {
@@ -102,64 +90,22 @@ public class BasicGameApp implements Runnable, KeyListener, MouseListener {
 			}
 		}
 
+		g.drawImage(image, xpos, ypos, 100, 100, null);
 		g.dispose();
-
 		bufferStrategy.show();
 	}
 
-	// REQUIRED KEYBOARD METHODS
 	public void keyPressed(KeyEvent e) {
-		char key = e.getKeyChar();
-		System.out.println("Key Pressed: " + key);
-
-		switch (key) {
-			case 'w':
-				up = true;
-				break;
-
-			case 's':
-				down = true;
-				break;
-
-			case 'a':
-				left = true;
-				break;
-
-			case 'd':
-				right = true;
-				break;
-		}
 
 	}
 
 	public void keyReleased(KeyEvent e) {
-		char key = e.getKeyChar();
-		System.out.println("Key Pressed: " + key);
 
-		switch (key) {
-			case 'w':
-				up = false;
-				break;
-
-			case 's':
-				down = false;
-				break;
-
-			case 'a':
-				left = false;
-				break;
-
-			case 'd':
-				right = false;
-				break;
-		}
 	}
 
 	public void keyTyped(KeyEvent e) {
-		// The getKeyModifiers method is a handy //way to get a String
-		// representing the //modifier key.
 		System.out.println("Key Typed: " + e.getKeyChar() + " "
-				+ KeyEvent.getKeyModifiersText(e.getModifiers()) + "\n");
+				+ InputEvent.getModifiersExText(e.getModifiersEx()) + "\n");
 	}
 
 	@Override
@@ -174,9 +120,9 @@ public class BasicGameApp implements Runnable, KeyListener, MouseListener {
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		System.out.println("test");
-		x = e.getX();
-		y = e.getY();
+		xdest = ((int) e.getX() / 100) * 100;
+		ydest = ((int) e.getY() / 100) * 100;
+		System.out.println("(" + xdest + ", " + ydest + ")");
 	}
 
 	@Override
