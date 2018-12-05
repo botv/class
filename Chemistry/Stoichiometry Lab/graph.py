da6a72201e09d39c4c4a73d5b7bf57b6154c6242
Ben Botvinick <benbotvinick@gmail.com>
2018-12-04 22:25:06 -0500

    chemistry work

---
+++ Chemistry/Stoichiometry Lab/graph.py
@@ -0,0 +1,26 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111, projection='3d')
+df = pd.read_csv('./data/data.csv')
+
+x = df.iloc[:,0]
+y = df.iloc[:,1]
+z1 = df.iloc[:,2]
+z2 = df.iloc[:,3]
+z3 = df.iloc[:,4]
+
+ax.scatter(x, y, z1)
+ax.scatter(x, y, z2)
+ax.scatter(x, y, z3)
+
+ax.set_xlabel('$NaOH$ (mL)')
+ax.set_ylabel('$CaCl_2$ (mL)')
+ax.set_zlabel('pH')
+
+plt.show()
+
